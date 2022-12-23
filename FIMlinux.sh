ed37910d8344fb5e753f2b0c70ab194f1f6f1821
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2022-12-23 21:21:48 +0530

    Update FIMlinux.sh

--- FIMlinux.sh
+++ FIMlinux.sh
@@ -8,16 +8,19 @@
 echo "Enter a directory path or file name:"
 read path
 
-# Create a baseline file containing the MD5 hashes of the specified files
-find "$path" -exec md5sum {} \; > baseline.txt
+# Get the current date and time in the format "DD-MM-YYYY_HH:MM:SS"
+current_datetime=$(date +%d-%m-%Y_%H:%M:%S)
+
+# Create a file named "hash_DD-MM-YYYY_HH:MM:SS.txt" containing the MD5 hashes of the specified files
+find "$path" -exec md5sum {} \; > "hash_$current_datetime.txt"
 
 # Continuously check the files for changes
 while true; do
     # Calculate the current MD5 hashes of the files
-    find "$path" -exec md5sum {} \; > current.txt
+    find "$path" -exec md5sum {} \; > "hash_$current_datetime.txt"
 
     # Compare the current hashes with the baseline
-    diff baseline.txt current.txt
+    diff "hash_$current_datetime.txt" "hash_$current_datetime.txt"
 
     # Sleep for 1 second before checking again
     sleep 1
