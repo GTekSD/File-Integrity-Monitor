f744696ddc8966c28afc91448fcbab154592a60f
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2022-12-23 20:41:49 +0530

    Update FIM.py

--- FIM.py
+++ FIM.py
@@ -1,26 +1,22 @@
-import hashlib
 import os
+import hashlib
 
 def calculate_file_hash(filepath):
     with open(filepath, 'rb') as f:
         data = f.read()
-    filehash = hashlib.sha512(data).hexdigest()
-    return filehash
+        return hashlib.sha512(data).hexdigest()
 
 def erase_baseline_if_already_exists():
     if os.path.exists('baseline.txt'):
+        # Delete it
         os.remove('baseline.txt')
 
-print('')
-print('What would you like to do?')
-print('')
-print('    A) Collect new Baseline?')
-print('    B) Begin monitoring files with saved Baseline?')
-print('')
-response = input('Please enter "A" or "B": ')
-print('')
+print("What would you like to do?")
+print("    A) Collect new Baseline?")
+print("    B) Begin monitoring files with saved Baseline?")
+response = input("Please enter 'A' or 'B': ")
 
-if response.upper() == 'A':
+if response.upper() == "A":
     # Delete baseline.txt if it already exists
     erase_baseline_if_already_exists()
 
@@ -30,17 +26,33 @@ if response.upper() == 'A':
 
     # For each file, calculate the hash, and write to baseline.txt
     for f in files:
-        filepath = 'Files/' + f
-        hash = calculate_file_hash(filepath)
+        filepath = os.path.join('Files', f)
+        hash_value = calculate_file_hash(filepath)
         with open('baseline.txt', 'a') as f:
-            f.write(f'{filepath}|{hash}\n')
+            f.write(f"{filepath}|{hash_value}\n")
 
-elif response.upper() == 'B':
+elif response.upper() == "B":
     file_hash_dictionary = {}
 
     # Load file|hash from baseline.txt and store them in a dictionary
     with open('baseline.txt', 'r') as f:
-        file_paths_and_hashes = f.readlines()
-    
-    for f in file_paths_and_hashes:
-        file_hash_dictionary[f.split('|')[0]] = f
+        for line in f:
+            filepath, hash_value = line.strip().split('|')
+            file_hash_dictionary[filepath] = hash_value
+
+    # Begin (continuously) monitoring files with saved Baseline
+    while True:
+        files = os.listdir('Files')
+
+        # For each file, calculate the hash and compare with the baseline
+        for f in files:
+            filepath = os.path.join('Files', f)
+            hash_value = calculate_file_hash(filepath)
+
+            # Notify if a new file has been created
+            if filepath not in file_hash_dictionary:
+                # A new file has been created!
+                print(f"{filepath} has been created!")
+
+                # Update the baseline
+                file_hash_dictionary[
